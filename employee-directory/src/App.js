affaa8899d4751c088b0863f24049fb8e1658bb3
NekoCarreraDesigns <denvermaas@gmail.com>
2020-08-07 22:06:06 -0600

    style the directory

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      Employee Directory
       <header className="App-header">
-        <input className="search-bar" type="text" />
+        <input className="search-bar" type="text" placeholder="Search Employee" />
         <button className="searchBtn" type="submit">Search</button>
       </header>
     </div>
