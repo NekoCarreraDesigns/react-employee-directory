88a7a36555554855575504ff1d97147319c7e788
NekoCarreraDesigns <denvermaas@gmail.com>
2020-08-08 17:14:20 -0600

    created utils folder, components folder, and Result.js

---
+++ employee-directory/src/utils/API.js
@@ -0,0 +1,11 @@
+import axios from "axios"
+const BASEURL = "https://randomuser.me/api/";
+const APIKEY = "&DUM1-N5W4-JCP4-QGP9";
+
+export default {
+    search: function (query) {
+        return axios.get(BASEURL + query + APIKEY);
+    }
+
+
+}
